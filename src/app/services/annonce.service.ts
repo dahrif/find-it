47693c6a4dcd64207c205c1fec632100a6062dad
Fatima Dahri <dahri.fatima.dev@gmail.com>
2022-06-17 16:47:08 +0100

    tabulations en cours

--- src/app/services/annonce.service.ts
+++ src/app/services/annonce.service.ts
@@ -68,6 +68,17 @@ export class AnnonceService {
       }))
  }
 
+  loadPerdus(){
+    return this.afs.collection('annonces', ref => ref.where('category', '==', 'Perdu')).snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, data }
+        })
+      }))
+ }
+
  loadOneData(id : any){
   return this.afs.doc(`annonces/${id}`).valueChanges();
 }
